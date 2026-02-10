5be2a700a9cda5d662843348a154c84aa0aa045f
David <davidbassouto@hotmail.com>
2026-02-10 16:56:38 -0300

    feat(server): add HTTP server with routes and response utilities

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/davidbassouto/ecommerce-golang/internal/config"
 	"github.com/davidbassouto/ecommerce-golang/internal/database"
 	"github.com/davidbassouto/ecommerce-golang/internal/logger"
+	"github.com/davidbassouto/ecommerce-golang/internal/server"
 	"github.com/gin-gonic/gin"
 )
 
@@ -25,6 +35,35 @@ func main() {
 	}
 	defer mainDb.Close()
 	gin.SetMode(cfg.Server.GinMode)
+
+	srv := server.NewServer(cfg, db, log)
+	router := srv.SetupRoutes()
+
+	httpsServer := &http.Server{
+		Addr:         fmt.Sprintf(":%s", cfg.Server.Port),
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		log.Info().Msgf("starting server on port %s", cfg.Server.Port)
+		if err := httpsServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal().Err(err).Msg("failed to start server")
+		}
+
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Info().Msg("shutdown signal received")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := httpsServer.Shutdown(ctx); err != nil {
+		log.Fatal().Err(err).Msg("failed to shutdown server")
+	}
 
 	log.Info().Msg("starting server")
 }
---
+++ internal/server/server.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/davidbassouto/ecommerce-golang/internal/config"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
+	"gorm.io/gorm"
+)
+
+type Server struct {
+	config *config.Config
+	db     *gorm.DB
+	logger zerolog.Logger
+}
+
+func NewServer(cfg *config.Config, db *gorm.DB, logger zerolog.Logger) *Server {
+	return &Server{
+		config: cfg,
+		db:     db,
+		logger: logger,
+	}
+}
+
+func (s *Server) SetupRoutes() *gin.Engine {
+	router := gin.New()
+
+	// add middlewares
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	router.Use(s.corsMiddleware())
+
+	//add routes
+	router.GET("/health", s.healthCheck)
+
+	return router
+}
+
+func (s *Server) healthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "OK",
+	})
+}
+
+func (s *Server) corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
