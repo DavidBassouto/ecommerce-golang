0226924ba110949c5ca6f6359a87d747684bbbbf
David <davidbassouto@hotmail.com>
2026-02-25 15:23:27 -0300

    feat(auth): implement authentication service with user registration and login

---
+++ internal/services/auth_servivce.go
@@ -0,0 +1,143 @@
+package services
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/davidbassouto/ecommerce-golang/internal/config"
+	"github.com/davidbassouto/ecommerce-golang/internal/dto"
+	"github.com/davidbassouto/ecommerce-golang/internal/models"
+	"github.com/davidbassouto/ecommerce-golang/internal/utils"
+	"gorm.io/gorm"
+)
+
+type AuthService struct {
+	db  *gorm.DB
+	cfg *config.Config
+}
+
+func newAuthService(db *gorm.DB, cfg *config.Config) *AuthService {
+	return &AuthService{
+		db:  db,
+		cfg: cfg,
+	}
+}
+
+func (s *AuthService) Register(req *dto.RegisterRequest) (*dto.AuthResponse, error) {
+
+	// check if user exists
+
+	var existingUser models.User
+	if err := s.db.Where("email = ?", req.Email).First(&existingUser).Error; err == nil {
+		return nil, errors.New("user not found")
+	}
+
+	// hash password
+	hashedPassword, err := utils.HashPassword(req.Password)
+	if err != nil {
+		return nil, err
+	}
+	// create user
+
+	user := models.User{
+		Email:     req.Email,
+		Password:  hashedPassword,
+		FirstName: req.FirstName,
+		LastName:  req.LastName,
+		Phone:     req.Phone,
+		Role:      models.UserRoleCustomer,
+	}
+	// result := s.db.Create(&user)
+	// if result.Error != nil {
+	// 	return nil, result.Error
+	// }
+	if err := s.db.Create(&user).Error; err != nil {
+		return nil, err
+	}
+
+	// create a cart
+	cart := models.Cart{
+		UserID: user.ID,
+	}
+	if err := s.db.Create(&cart).Error; err != nil {
+		// return nil, err
+		fmt.Println("Unable to create a cart")
+	}
+
+	// generate token
+	return s.genereteAuthResponse(&user)
+}
+
+func (s *AuthService) Login(req *dto.LoginRequest) (*dto.AuthResponse, error) {
+	// check if user exists
+	var user models.User
+	if err := s.db.Where("email = ? AND is_active = ?", req.Email, true).First(&user).Error; err != nil {
+		return nil, errors.New("Invalid credentials")
+	}
+	// check password
+	if !utils.CheckPassword(req.Password, user.Password) {
+		return nil, errors.New("invalid password")
+	}
+	// generate token
+	return s.genereteAuthResponse(&user)
+}
+
+func (s *AuthService) RefreshToken(req *dto.RefreshTokenRequest) (*dto.AuthResponse, error) {
+	claims, err := utils.ValidateToken(req.RefreshToken, s.cfg.JWT.SecretKey)
+	if err != nil {
+		return nil, errors.New("Invalid refresh token ")
+	}
+	var refreshToken models.RefreshToken
+	if err := s.db.Where("token = ? AND expires_at > ?", req.RefreshToken, time.Now()).First(&refreshToken).Error; err != nil {
+		return nil, errors.New("Refresh token invalid or not found")
+	}
+
+	var user models.User
+	if err := s.db.First(&user, claims.UserID).Error; err != nil {
+		return nil, errors.New("User not found")
+	}
+
+	s.db.Delete(&refreshToken)
+
+	return s.genereteAuthResponse(&user)
+}
+
+func (s *AuthService) Logout(refreshToken string) error {
+	return s.db.Where("token = ?", refreshToken).Delete(&models.RefreshToken{}).Error
+}
+
+func (s *AuthService) genereteAuthResponse(user *models.User) (*dto.AuthResponse, error) {
+	accessToken, refreshToken, err := utils.GenerateTokenPair(
+		&s.cfg.JWT,
+		user.ID,
+		user.Email,
+		string(user.Role))
+	if err != nil {
+		return nil, err
+	}
+
+	// save refresh token
+	refreshTokenModel := models.RefreshToken{
+		Token:     refreshToken,
+		UserID:    user.ID,
+		ExpiresAt: time.Now().Add(s.cfg.JWT.RefreshExpiresIn),
+	}
+	if err := s.db.Create(&refreshTokenModel).Error; err != nil {
+		return nil, err
+	}
+
+	return &dto.AuthResponse{
+		User: dto.UserResponse{
+			ID:        user.ID,
+			Email:     user.Email,
+			FirstName: user.FirstName,
+			LastName:  user.LastName,
+			Phone:     user.Phone,
+			Role:      string(user.Role),
+			IsActive:  user.IsActive,
+		},
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}, nil
+}
